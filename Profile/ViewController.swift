94479aeefe8994a5238fb8bd32b4f71af730cd44
Ayanon Fukui <cainaifujing49@gmail.com>
2020-05-11 14:22:09 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 福井彩乃 on 2020/05/11.
+//  Copyright © 2020 Fukui Ayanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
