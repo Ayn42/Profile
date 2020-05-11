5f98e0ead4417f7ff1051329f34f52e245a27f9a
Ayanon Fukui <cainaifujing49@gmail.com>
2020-05-11 17:07:31 +0900

    プロフィールアプリ応用の完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,21 +11,47 @@ import UIKit
 class ViewController: UIViewController {
     //画像を表示する変数を作る
     @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileLabel: UILabel!
     
     //ラベルを表示する変数を作る
     @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //画像を見えないようにする
-        profileImageView.isHidden = true
-        //ラベルを見えないようにする
-        profileCommentLabel.isHidden = true}
+       }
 //ボタンがタップされた時に
     @IBAction func tapButton(){
-        //画像を見える状態にする
-        profileImageView.isHidden = false
-        //ラベルを見える状態にする
-        profileCommentLabel.isHidden = false}
+        //画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "philImage")
+        //ラベルをLabelに反映する
+        profileLabel.text = "名前"
+        //コメントをLabelに反映する
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+        }
+//ボタンがタップされた時に
+    @IBAction func tapButton2(){
+        //画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "trackImage")
+        //ラベルをLabelに反映する
+        profileLabel.text = "スポーツ"
+        //コメントをLabelに反映する
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+    }
+    @IBAction func tapButton3(){
+           //画像をImageViewに反映する
+           profileImageView.image = UIImage(named: "appleImage")
+           //ラベルをLabelに反映する
+           profileLabel.text = "好きな食べ物"
+           //コメントをLabelに反映する
+           profileCommentLabel.text = "リンゴが好きで、いつも持ち歩いている"
+       }
+    @IBAction func tapButton4(){
+           //画像をImageViewに反映する
+           profileImageView.image = UIImage(named: "flightImage")
+           //ラベルをLabelに反映する
+           profileLabel.text = "趣味"
+           //コメントをLabelに反映する
+           profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+       }
 }
 
